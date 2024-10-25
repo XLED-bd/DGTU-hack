f4f6189f2bffcb8dda97c99cb9e6caf838eaba1b
stepa <stepanenkofrol@gmail.com>
2024-10-25 18:15:44 +0300

    test

--- app/src/main/java/com/code_wizards/ecology/MainActivity.kt
+++ app/src/main/java/com/code_wizards/ecology/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             EcologyTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "glen",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
